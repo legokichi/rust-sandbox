aab8730ea530b3bc6e0a4f86272d140c23bb0a00
Legokichi Duckscallion <legokichi@gmail.com>
2019-09-28 11:11:46 +0900

    update

--- aws-dynamodb-query-builder/src/main.rs
+++ aws-dynamodb-query-builder/src/main.rs
@@ -1,32 +1,46 @@
 #![feature(async_await)]
+#![allow(unused_imports)]
+
+#[macro_use]
+extern crate serde_dynamodb_derive;
 
 use serde::{Serialize, Deserialize};
 use serde_json::json;
-use futures::compat::Future01CompatExt as _;
+
+use futures::compat::{Future01CompatExt as _, Stream01CompatExt as _};
+
+use dynomite::DynamoDbExt;
+
+use dynomite::{Item, FromAttributes, Attributes};
+
 use rusoto_core::Region;
 use rusoto_dynamodb::{DynamoDb, DynamoDbClient};
+use rusoto_dynamodb::{QueryInput, QueryOutput, AttributeValue};
+
+use serde_dynamodb::ToQueryInput;
+
 use std::error::Error;
+
 use log::info;
+
+
 
 #[derive(Deserialize, Debug, Clone)]
 struct Config {
     pub permanent_table_name: String,
 }
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(tag = "version")]
-enum DeviceTable {
-    DeviceTable20190228(DeviceTable20190228),
-    DeviceTable20190707(DeviceTable20190707),
-}
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct DeviceTable20190228 {
+
+#[derive(Serialize, Deserialize, Item, ToQueryInput, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Person {
     pub hash_key: String,
     pub range_key: String,
 }
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct DeviceTable20190707 {
-    pub hash_key: String,
-    pub range_key: String,
+
+fn list_device_query(o: ListDeviceRequest) -> QueryInput {
+
+}
+fn parse_device_query(q: QueryOutput) -> Vec<Device> {
+
 }
 
 #[runtime::main(runtime_tokio::Tokio)]
@@ -35,22 +49,17 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     env_logger::try_init().ok();
     let config = envy::from_env::<Config>()?;
 
-
     let client = DynamoDbClient::new(Region::default());
-    use rusoto_dynamodb::{QueryInput, QueryOutput, AttributeValue};
-    let table = DeviceTable::DeviceTable20190228(DeviceTable20190228{hash_key: "a".to_string(), range_key: "b".to_string()});
+    
     let QueryOutput {
         items,
         ..
-    } = client.query(QueryInput{
-        table_name: config.permanent_table_name,
-        key_condition_expression: Some("#hash_key = :hash_key".to_string()),
-        expression_attribute_names: Some(serde_json::from_value(json!({
-            "#hash_key": "hash_key",
-        }))?),
-        expression_attribute_values: Some(serde_dynamodb::to_hashmap(&table)?),
-        ..Default::default()
-    }).compat().await?;
+    } = client.query(
+        PersonQueryInput{
+            hash_key: Some("a".to_string()),
+            ..Default::default()
+        }.to_query_input(config.permanent_table_name)
+    ).compat().concat().await?;
     info!("{:?}", items);
 
     Ok(())
