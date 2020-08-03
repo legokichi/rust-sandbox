57b00806ba8a41cefee7929df97e1355c33df89b
Legokichi Duckscallion <legokichi@gmail.com>
2020-08-04 05:20:51 +0900

    fmt

--- naive-bayse-spam-filter/src/main.rs
+++ naive-bayse-spam-filter/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 fn main() {
     let valid_mails = vec![r##"a b c"##, r##"d e f"##];
-    let invalid_mails = vec![r##"u v w"##, r##"x y z"##];
+    let invalid_mails = vec![/*r##"u v w"##,*/ r##"x y z"##];
     let test_mails = vec![
         r##"x y z x x y u v w x"##,
         r##"a c b c x c y c b e b z"##,
@@ -38,6 +38,23 @@ fn main() {
         });
         let is_spam = dbg!(dbg!((p_invalid / p_valid).ln()) + dbg!(oo)) > 1_f64;
         dbg!((test_mail, is_spam));
+        let valid_score = tokens.iter().fold(dbg!(p_valid.ln()), |a, k| {
+            let valid = p_f_valid.get(*k).unwrap_or(&0.0000001_f64) + 0_f64;
+            // dbg!((k, valid, (valid).ln(), a + valid.ln()));
+            a + valid.ln()
+        });
+        let invalid_score = tokens.iter().fold(dbg!(p_invalid.ln()), |a, k| {
+            let invalid = p_f_invalid.get(*k).unwrap_or(&0.0000001_f64) + 0_f64;
+            // dbg!((k, invalid, (invalid).ln(), a + invalid.ln()));
+            a + invalid.ln()
+        });
+        dbg!((
+            test_mail,
+            valid_score,
+            invalid_score,
+            1_f64.exp().powf(valid_score),
+            1_f64.exp().powf(invalid_score)
+        ));
     }
 }
 fn normalize(table: &HashMap<String, u64>) -> HashMap<String, f64> {
