9102e01e7c809b4fe18ba60dea9cae904e185706
Legokichi Duckscallion <legokichi@gmail.com>
2022-06-08 23:04:56 +0900

    wip

--- grcov-sandbox/src/main.rs
+++ grcov-sandbox/src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+fn hoge(){
+    println!("hoge");
+}
+fn huga(){
+    println!("huga");
+}
 #[test]
 fn test(){
+    hoge();
+    huga();
     assert!(true);
 }
 
