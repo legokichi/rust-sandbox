ef3edeca0148ba287586a8c5d88d4e5df61f2247
Legokichi Duckscallion <legokichi@gmail.com>
2024-08-22 05:20:05 +0900

    wip

--- litestream-sandbox/src/model/api/mod.rs
+++ litestream-sandbox/src/model/api/mod.rs
@@ -1,18 +1,28 @@
+pub mod list_access_logs;
+pub mod list_river_waypoints;
+pub mod list_rivers;
+pub mod list_users;
+// pub mod update_river_waypoint;
+pub mod create_river_waypoint;
+
 #[derive(
     Debug,
     serde::Deserialize,
     serde::Serialize,
     Clone,
     PartialEq,
-    Eq,
     derive_more::TryInto,
     derive_more::From,
 )]
 #[serde(tag = "type")]
 #[serde(rename_all = "camelCase")]
 pub enum Request {
-    ListUser(crate::model::api::list_user::Request),
+    ListUsers(crate::model::api::list_users::Request),
     ListAccessLogs(crate::model::api::list_access_logs::Request),
+    ListRivers(crate::model::api::list_rivers::Request),
+    ListRiverWaypoints(crate::model::api::list_river_waypoints::Request),
+    // UpdateRiverWaypoint(crate::model::api::update_river_waypoint::Request),
+    CreateRiverWaypoint(crate::model::api::create_river_waypoint::Request),
 }
 
 #[derive(
@@ -21,15 +31,18 @@ pub enum Request {
     serde::Serialize,
     Clone,
     PartialEq,
-    Eq,
     derive_more::TryInto,
     derive_more::From,
 )]
 #[serde(tag = "type")]
 #[serde(rename_all = "camelCase")]
 pub enum Response {
-    ListUser(crate::model::api::list_user::Response),
+    ListUser(crate::model::api::list_users::Response),
     ListAccessLogs(crate::model::api::list_access_logs::Response),
+    ListRivers(crate::model::api::list_rivers::Response),
+    ListRiverWaypoints(crate::model::api::list_river_waypoints::Response),
+    // UpdateRiverWaypoint(crate::model::api::update_river_waypoint::Response),
+    CreateRiverWaypoint(crate::model::api::create_river_waypoint::Response),
     Error(ErrorKind),
 }
 
@@ -42,7 +55,8 @@ pub enum ErrorKind {
 }
 
 impl Request {
-    pub fn to_request_type(&self) -> Result<String, anyhow::Error> {
+    #[allow(dead_code)]
+    pub fn to_request_type_string(&self) -> Result<String, anyhow::Error> {
         use anyhow::Context;
         let json = serde_json::to_value(self)?;
         let json = json.pointer(".type").context("type field not found")?;
@@ -52,49 +66,12 @@ impl Request {
 }
 
 impl Response {
-    pub fn to_response_type(&self) -> Result<String, anyhow::Error> {
+    #[allow(dead_code)]
+    pub fn to_response_type_string(&self) -> Result<String, anyhow::Error> {
         use anyhow::Context;
         let json = serde_json::to_value(self)?;
         let json = json.pointer(".type").context("type field not found")?;
         let txt = json.as_str().context("type field is not a string")?;
         Ok(txt.to_string())
-    }
-}
-
-pub mod list_user {
-    #[derive(Debug, serde::Deserialize, serde::Serialize, Clone, PartialEq, Eq)]
-    #[serde(rename_all = "camelCase")]
-    pub struct Request {
-        #[serde(skip_serializing_if = "Option::is_none")]
-        pub offset: Option<u32>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        pub limit: Option<u32>,
-    }
-
-    #[derive(Debug, serde::Deserialize, serde::Serialize, Clone, PartialEq, Eq)]
-    #[serde(rename_all = "camelCase")]
-    pub struct Response {
-        pub users: Vec<crate::model::user::User>,
-        pub next: u32,
-    }
-}
-
-pub mod list_access_logs {
-    #[derive(Debug, serde::Deserialize, serde::Serialize, Clone, PartialEq, Eq)]
-    #[serde(rename_all = "camelCase")]
-    pub struct Request {
-        #[serde(skip_serializing_if = "Option::is_none")]
-        pub offset: Option<u32>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        pub limit: Option<u32>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        pub user_id: Option<i64>,
-    }
-
-    #[derive(Debug, serde::Deserialize, serde::Serialize, Clone, PartialEq, Eq)]
-    #[serde(rename_all = "camelCase")]
-    pub struct Response {
-        pub access_logs: Vec<crate::model::user::AccessLog>,
-        pub next: u32,
     }
 }
